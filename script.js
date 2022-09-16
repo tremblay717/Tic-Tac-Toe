20006fd8a42a97629c600f4013aa67ed2af8115b
christophe <tremblay717@gmail.com>
2022-09-16 08:34:33 -0400

    added player X and O

--- script.js
+++ script.js
@@ -19,12 +19,8 @@ const Gameboard = {
         for (let i = 0; i < 9; i++) {
             const box = document.createElement('box');
             box.className = 'gridBox';
+            box.style.backgroundColor = "yellow"
             box.id = i + "box";
-            if(i==3){
-                const test = document.createElement('p');
-                test.textContent = 'test'
-                box.appendChild(test)
-            }
             grid.appendChild(document.createElement('box'));
             gameArray.push(box);
         }
@@ -42,9 +38,29 @@ const tile = {
 const playerOne = {
     name: 'Player One',
     playerOneText: function () {
+
         const playerOneText = document.createElement('p');
         playerOneText.className = 'player';
         playerOneText.textContent = playerOne.name;
+
+        const tileDiv = document.createElement('div')
+        tileDiv.className = 'tileDiv';
+        playerOneText.appendChild(tileDiv)
+
+        const x = document.createElement('span');
+        x.id = 'playerOneX'
+        x.className = 'tile';
+        x.style.backgroundColor= 'white';
+        x.style.color='black';
+        x.textContent = tile.x;
+        tileDiv.appendChild(x);
+
+        const O = document.createElement('span');
+        O.className = 'tile';
+        O.style.backgroundColor='white';
+        O.style.color='black';
+        O.textContent = tile.o;
+        tileDiv.appendChild(O);
 
         return gameDiv.appendChild(playerOneText);
     }
@@ -53,15 +69,34 @@ const playerOne = {
 const playerTwo = {
     name: 'Player Two',
     playerTwoText: function () {
-        const playerTwoDiv = document.createElement('div');
         const playerTwoText = document.createElement('p');
-        playerTwoDiv.className = 'player';
+        playerTwoText.className = 'player';
         playerTwoText.textContent = playerTwo.name;
-        playerTwoDiv.appendChild(playerTwoText)
-        return gameDiv.appendChild(playerTwoDiv);
+
+        const tileDiv = document.createElement('div')
+        tileDiv.className = 'tileDiv';
+        playerTwoText.appendChild(tileDiv)
+
+        const x = document.createElement('span');
+        x.className = 'tile';
+        x.style.backgroundColor= 'white';
+        x.style.color='black';
+        x.textContent = tile.x;
+        tileDiv.appendChild(x);
+
+        const O = document.createElement('span');
+        O.className = 'tile';
+        O.style.backgroundColor='white';
+        O.style.color='black';
+        O.textContent = tile.o;
+        tileDiv.appendChild(O);
+
+        return gameDiv.appendChild(playerTwoText);
     }
 }
 
 playerOne.playerOneText();
 Gameboard.gameBoard();
 playerTwo.playerTwoText();
+
+const playerOneX = document.getElementById('playerOneX')
